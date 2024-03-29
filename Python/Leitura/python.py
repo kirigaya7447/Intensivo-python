15ee59fd54d9cc6f0b6ab2782d2453aeb05ec14b
kirigaya7447 <joaoh2915@gmail.com>
2024-03-28 21:08:59 -0300

    Reorganizando diretórios

--- Python/Leitura/python.py
+++ Python/Leitura/python.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-df_principal = pd.read_excel("/home/joao4774/Área de Trabalho/GitHub/Intensivo-python/Python/Leitura/Planilha.xlsx", sheet_name="Principal")
+df_principal = pd.read_excel("/home/joao4774/Área de Trabalho/GitHub/Intensivo-python/Planilha/Planilha.xlsx", sheet_name="Principal")
 print(df_principal.head(5))
 
-df_acoes = pd.read_excel("/home/joao4774/Área de Trabalho/GitHub/Intensivo-python/Python/Leitura/Planilha.xlsx", sheet_name="Total_de_acoes")
+df_acoes = pd.read_excel("/home/joao4774/Área de Trabalho/GitHub/Intensivo-python/Planilha/Planilha.xlsx", sheet_name="Total_de_acoes")
 print(df_acoes.head(7))
 
-df_ticker = pd.read_excel("/home/joao4774/Área de Trabalho/GitHub/Intensivo-python/Python/Leitura/Planilha.xlsx", sheet_name="Ticker")
+df_ticker = pd.read_excel("/home/joao4774/Área de Trabalho/GitHub/Intensivo-python/Planilha/Planilha.xlsx", sheet_name="Ticker")
 print(df_ticker)
