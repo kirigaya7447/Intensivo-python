633a62b393545c1bed54b1986c509309e4886296
kirigaya7447 <joaoh2915@gmail.com>
2024-04-01 20:47:50 -0300

    Começando organização dos arquivos.py

---
+++ Python/Graficos/Barras/python.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import plotly.express as pe
+
+pd.options.display.float_format = "{:,.2f}".format
+
+planilha = "/home/joao4774/Área de Trabalho/GitHub/Intensivo-python/Planilha/Planilha.xlsx"
+
+df_principal = pd.read_excel(planilha, sheet_name="Principal")
+
+df_analise_saldo = df_principal.groupby("Resultado:")["Variação (R$):"].sum().reset_index()
+print(df_analise_saldo)
+
+grafico = pe.bar(df_analise_saldo, x="Resultado:", y="Variação (R$):", text="Variação (R$):", title="Variação X Resultado")
+grafico.show()
+
+df_analise_saldo = pe.data.tips()
+grafico = pe.pie(df_analise_saldo, values="tip", names="day", title="Hello!")
+grafico.show()
