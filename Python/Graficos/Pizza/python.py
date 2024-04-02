01f0e5aa067436756676a8dad388ac3744c2fa1c
kirigaya7447 <joaoh2915@gmail.com>
2024-04-01 22:10:58 -0300

    Arrumando o gráfico de pizza

--- Python/Graficos/Pizza/python.py
+++ Python/Graficos/Pizza/python.py
@@ -11,9 +11,8 @@ print(df_chat)
 df_idade = df_chat.groupby(["Idade (anos):"]).sum().reset_index()
 df_idade = df_idade.drop(columns=["Empresa:"])
 df_idade = df_idade.drop(columns=["Segmento:"])
-df_idade["index"] = df_idade.index
 print(df_idade)
 
 
-grafico = pe.pie(values=df_idade["Idade (anos):"], names=df_idade["index"], title="Hello!")
+grafico = pe.pie(values=df_idade["Idade (anos):"], names=df_idade["Idade (anos):"], title="Porcentagem de idades das empresas:")
 grafico.show()
