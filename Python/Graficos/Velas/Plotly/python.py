ece03098282198f825e4e196341bf8b3c4bdbd8d
kirigaya7447 <joaoh2915@gmail.com>
2024-04-01 21:50:45 -0300

    Retirando importações não utilizadas no código

--- Python/Graficos/Velas/Plotly/python.py
+++ Python/Graficos/Velas/Plotly/python.py
@@ -1,7 +1,5 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-import mplfinance as mpf
 import yfinance as yf
 import plotly.graph_objects as go 
 from plotly.subplots import make_subplots
