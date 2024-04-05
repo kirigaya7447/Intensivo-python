cd56b9fcf4d2f59ec4a3ff68e0531006ed046e08
kirigaya7447 <joaoh2915@gmail.com>
2024-04-04 21:11:58 -0300

    Comentários em renomeado py

--- Python/Acoes/Renomear/python.py
+++ Python/Acoes/Renomear/python.py
@@ -4,8 +4,12 @@ planilha = "/home/joao4774/Área de Trabalho/GitHub/Intensivo-python/Planilha/Pl
 
 df_principal = pd.read_excel(planilha, sheet_name="Principal")
 
+#realiza uma cópia apenas das colunas apresentadas
 df_principal = df_principal[["Ativo", "Data", "Último (R$)", "Var. Dia (%)"]].copy()
 
+print("Colunas cruas: \n", df_principal)
+
+#renomeia as colunas e copia
 df_principal = df_principal.rename(columns={"Último (R$)": "Ultimo(R$)", "Var. Dia (%)": "Var_Dia(%)"}).copy()
 
-print(df_principal)
+print("Colunas renomeadas: \n", df_principal)
