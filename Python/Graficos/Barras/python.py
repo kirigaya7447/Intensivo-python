144ca7219a0664cf179dbdba62bed82df0efcf51
kirigaya7447 <joaoh2915@gmail.com>
2024-04-04 22:32:13 -0300

    Arrumado o nome da coluna em Barras

--- Python/Graficos/Barras/python.py
+++ Python/Graficos/Barras/python.py
@@ -21,7 +21,7 @@ menos_de_cinquenta = df_principal["Categoria de idade:"].value_counts()["Menos d
 #conta quantas empresas tem entre 50 e 100 anos
 entre = df_principal["Categoria de idade:"].value_counts()["Empresa de 50 a 100 anos"]
 #cria um dataframe com os valores acima
-coluna_idade = pd.DataFrame([["Empresas com mais de 100 anos", mais_de_cem], ["Empresas com menos de 50 anos", menos_de_cinquenta], ["Empresas entre os 50 e 100 anos", entre]], columns=["Variacao","Idade"])
+coluna_idade = pd.DataFrame([["Empresas com mais de 100 anos", mais_de_cem], ["Empresas com menos de 50 anos", menos_de_cinquenta], ["Empresas entre os 50 e 100 anos", entre]], columns=["Intervalo","Idade"])
 #configura e formata o grafico
-grafico_idade = pe.bar(coluna_idade, y="Idade", x="Variacao", title="Idade de empresas")
+grafico_idade = pe.bar(coluna_idade, y="Idade", x="Intervalo", title="Idade de empresas")
 grafico_idade.show()
