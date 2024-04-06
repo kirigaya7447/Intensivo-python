9d7e682e1fbd1179542a3300984fd4b042f1eb49
kirigaya7447 <joaoh2915@gmail.com>
2024-04-05 22:22:47 -0300

    Comentários no pizza

--- Python/Graficos/Pizza/python.py
+++ Python/Graficos/Pizza/python.py
@@ -7,15 +7,18 @@ planilha = "/home/joao4774/Área de Trabalho/GitHub/Intensivo-python/Planilha/Pl
 
 df_principal = pd.read_excel(planilha, sheet_name="Principal")
 
+#pega a coluna Segmento
 df_analise = df_principal["Segmento:"]
+#adiciona a coluna Variação
 df_analise = pd.concat([df_analise, df_principal["Variação (R$):"]], axis=1)
-df_analise = pd.concat([df_analise, df_principal["Resultado:"]], axis=1)
 
-
+#agrupa pelos segmentos que forem iguais e soma a Variação de cada um
 df_analise = df_analise.groupby("Segmento:")["Variação (R$):"].sum().reset_index()
+#converte os negativos para positivos
 df_analise["Variação (R$):"] = df_analise["Variação (R$):"].apply(lambda cont: cont * -1 if cont < 0 else(cont * 1))
 
 print("Dados a serem analisados: \n", df_analise)
 
+#cria o grafico em formato de "Torta" ou "Pizza"
 grafico = pe.pie(values=df_analise["Variação (R$):"], names=df_analise["Segmento:"], title="Segmentos e suas respectivas ações:")
 grafico.show()
