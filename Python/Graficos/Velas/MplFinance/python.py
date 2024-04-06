73d8772e7740209bd1afa39f91551471f06628fa
kirigaya7447 <joaoh2915@gmail.com>
2024-04-06 11:45:26 -0300

    Comentários em Velas MPLFinance

--- Python/Graficos/Velas/MplFinance/python.py
+++ Python/Graficos/Velas/MplFinance/python.py
@@ -1,6 +1,12 @@
 import mplfinance as mpf
 import yfinance as yf
 
+#download dos dados
 dados = yf.download("PETR4.SA", start="2023-01-01", end="2024-01-01")
 
-mpf.plot(dados.head(30), type="candle", figsize=(10,6), volume=True, mav=(7,14), style="brasil")
+#cria gráfico com a api do tipo candle, define tamanho. Volume é um segundo gráfico de 
+#barras que verifica a abertura e o fechamento. O mav é a média móvel que é de 7 e 14 dias
+#e o style é apenas cor
+mpf.plot(dados.head(90), type="candle", figsize=(10,6), volume=True, mav=(7, 14), style="brasil")
+
+mpf.plot(dados.head(360), type="line", figsize=(10,6), volume=True, mav=(7, 14, 30), style="yahoo")
