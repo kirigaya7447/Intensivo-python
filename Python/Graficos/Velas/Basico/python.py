a70ef42d59f4aa0968e00d54f1fe2f557d8304d2
kirigaya7447 <joaoh2915@gmail.com>
2024-04-05 23:52:26 -0300

    Fazendo o Basico do Velas

--- Python/Graficos/Velas/Basico/python.py
+++ Python/Graficos/Velas/Basico/python.py
@@ -2,14 +2,20 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import yfinance as yf
 
+#faz download dos dados do yahoo finance
 dados = yf.download("PETR4.SA", start="2023-01-01", end="2024-01-01")
+
+#coleta os dados
 colunas_origin = dados.columns
+#renomeia as colunas
 dados.columns = ["Abertura", "Maximo", "Minimo", "Fechamento", "Ajuste de fechamento", "Volume"]
+
+#renomeia o id
 dados = dados.rename_axis("Data")
 print(dados)
 
-dados["Fechamento"].plot(figsize=(10, 6))
-plt.title("variação do preço por data")
+dados["Fechamento"].plot(figsize=(10,6))
+plt.title("Variação do preço por data")
 plt.legend("Fechamento")
 
 plt.show()
