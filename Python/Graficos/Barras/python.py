eeef0a4cbdb47287abacbf6ee8b7a76534bbe581
kirigaya7447 <joaoh2915@gmail.com>
2024-04-06 13:33:40 -0300

    Estilizando e completado os desafios

--- Python/Graficos/Barras/python.py
+++ Python/Graficos/Barras/python.py
@@ -11,7 +11,7 @@ df_principal = pd.read_excel(planilha, sheet_name="Principal")
 df_analise_saldo = df_principal.groupby("Resultado:")["Variação (R$):"].sum().reset_index()
 print("Tabela com a somatória de descida e subida: \n", df_analise_saldo)
 
-grafico = pe.bar(df_analise_saldo, x="Resultado:", y="Variação (R$):", text="Variação (R$):", title="Variação X Resultado")
+grafico = pe.bar(df_analise_saldo, x="Resultado:", y="Variação (R$):", text="Variação (R$):", title="Variação X Resultado", color="Variação (R$):")
 grafico.show()
 
 #conta quantas empresas tem mais de 100 anos
@@ -22,8 +22,8 @@ menos_de_cinquenta = df_principal["Categoria de idade:"].value_counts()["Menos d
 entre = df_principal["Categoria de idade:"].value_counts()["Empresa de 50 a 100 anos"]
 
 #cria um dataframe com os valores acima
-coluna_idade = pd.DataFrame([["Empresas com mais de 100 anos", mais_de_cem], ["Empresas com menos de 50 anos", menos_de_cinquenta], ["Empresas entre os 50 e 100 anos", entre]], columns=["Intervalo","Idade"])
+coluna_idade = pd.DataFrame([["Empresas com mais de 100 anos", mais_de_cem], ["Empresas com menos de 50 anos", menos_de_cinquenta], ["Empresas entre os 50 e 100 anos", entre]], columns=["Intervalo","Quantidade"])
 
 #configura e formata o grafico
-grafico_idade = pe.bar(coluna_idade, y="Idade", x="Intervalo", title="Idade de empresas")
+grafico_idade = pe.bar(coluna_idade, y="Quantidade", x="Intervalo", title="Quantidade X Idade de empresas", color="Intervalo")
 grafico_idade.show()
--- Python/Graficos/Velas/MplFinance/python.py
+++ Python/Graficos/Velas/MplFinance/python.py
@@ -3,10 +3,13 @@ import yfinance as yf
 
 #download dos dados
 dados = yf.download("PETR4.SA", start="2023-01-01", end="2024-01-01")
+dados_apple = yf.download("AAPL", start="2023-01-01", end="2024-01-01")
 
 #cria gráfico com a api do tipo candle, define tamanho. Volume é um segundo gráfico de 
-#barras que verifica a abertura e o fechamento. O mav é a média móvel que é de 7 e 14 dias
+#barras que verifica o volume. O mav é a média móvel que é de 7 e 14 dias
 #e o style é apenas cor
 mpf.plot(dados.head(90), type="candle", figsize=(10,6), volume=True, mav=(7, 14), style="brasil")
 
 mpf.plot(dados.head(360), type="line", figsize=(10,6), volume=True, mav=(7, 14, 30), style="yahoo")
+
+mpf.plot(dados_apple.head(180), type="candle", figsize=(10,6), volume=True, mav=(30), style="yahoo")
