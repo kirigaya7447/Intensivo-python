42da44f10c60fa0a3bca76a9b550138f6e2a70d6
kirigaya7447 <joaoh2915@gmail.com>
2024-04-06 11:06:53 -0300

    Comentários no velas basico py

--- Python/Graficos/Velas/Basico/python.py
+++ Python/Graficos/Velas/Basico/python.py
@@ -12,15 +12,22 @@ dados.columns = ["Abertura", "Maximo", "Minimo", "Fechamento", "Ajuste de fecham
 
 #renomeia o id
 dados = dados.rename_axis("Data")
-print(dados)
+print("Dados para análise: \n", dados)
 
+#define o intervalo de dados como sendo da coluna Fechamento e o plot faz criar a imagem e 
+#define que o tamanho será de 10 X 6
 dados["Fechamento"].plot(figsize=(10,6))
+#dá título pro gráfico
 plt.title("Variação do preço por data")
+#Adiciona legenda
 plt.legend("Fechamento")
-
+#mostra o gráfico
 plt.show()
 
+#lê 60 linhas de dados e copia
 df = dados.head(60).copy()
+#cria coluna Data
 df["Data"] = df.index
+#altera o formato do Data para um compreensível para o matplotlib
 df["Data"] = df["Data"].apply(mdates.date2num)
 print(df)
